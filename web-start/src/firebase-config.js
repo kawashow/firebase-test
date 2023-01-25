0e17da7af4ba94eff0243dd38b1368efbbe8a11e
kawashow <ksks17179@gmail.com>
2023-01-25 12:34:14 +0900

    #7: ユーザのサインイン設定まで反映

--- web-start/src/firebase-config.js
+++ web-start/src/firebase-config.js
@@ -6,15 +6,39 @@
  * 3. Select Config from the Firebase SDK snippet pane.
  * 4. Copy the config object snippet, then add it here.
  */
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+
 const config = {
   /* TODO: ADD YOUR FIREBASE CONFIGURATION OBJECT HERE */
+  
 };
 
 export function getFirebaseConfig() {
-  if (!config || !config.apiKey) {
+  if (!firebaseConfig || !firebaseConfig.apiKey) {
     throw new Error('No Firebase configuration object provided.' + '\n' +
     'Add your web app\'s configuration object to firebase-config.js');
   } else {
-    return config;
+    return firebaseConfig;
   }
 }
